2823f52fa54ed937b6a380114308a17db0cc79b6
leeynyker <leeynykeramr@gmail.com>
2022-06-02 18:57:38 -0500

    clase 3

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,30 @@
     <p v-html="textoHtml"></p>
     <input type="text" v-model="inputText">
     <img :src="img" alt="">
+    <br>
+    <br>
+    <br>
+    <button @click="incrementarContador()">
+      Botón {{contador}}
+    </button>
+    <input type="text" @keyup="presionarTecla()" v-model="inputEvent">
+    <select @change="cambiarColor()" v-model="colorContenedor">
+      <option value="contenedor verde">verde</option>
+      <option value="contenedor rojo">rojo</option>
+      <option value="contenedor amarillo">amarillo</option>
+    </select>
+    <br>
+    <div :class="contenedor" ></div>
+    <br>
+    <br>
+    <form @submit="enviarFormulario" >
+      <!-- <input type="text" v-model="inputForm"> -->
+      <textarea cols="30" rows="10" v-model="inputForm"></textarea>
+      <input type="submit" value="Enviar">
+    </form>
+    <input type="text" @input="cambiarParrafo" v-model="inputForm">
+    <br>
+    <p> {{parrafo}} </p>
   </div>
 </template>
 <script>
@@ -18,6 +42,30 @@ export default {
       inputText: " Escribe un texto",
       img: "https://picsum.photos/300/300",
       clasePruebas: "texto-rojo",
+      // clase 3
+      contador: 0,
+      inputEvent: "",
+      contenedor: "contenedor azul",
+      colorContenedor: "contenedor verde",
+      inputForm: "",
+      parrafo: "",
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++;
+    },
+    presionarTecla() {
+      alert(this.inputEvent);
+    },
+    cambiarColor() {
+      this.contenedor = this.colorContenedor;
+    },
+    enviarFormulario(){
+      this.parrafo = this.inputForm;
+    },
+    cambiarParrafo(){
+      this.parrafo = this.inputForm;
     }
   },
 }
@@ -29,5 +77,21 @@ export default {
 .texto-rojo{
   color: red;
   background-color: black;
+}
+.contenedor{
+  width: 100px;
+  height: 100px;
+}
+.azul{
+  background-color: blue;
+}
+.verde{
+  background-color: green;
+}
+.rojo{
+  background-color: red;
+}
+.amarillo{
+  background-color: yellow;
 }
 </style>
